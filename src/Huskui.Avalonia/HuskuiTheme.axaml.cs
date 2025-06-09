43dd0f74b69698f5f7c8089a2a5c48c5732ce35c
Dearain <d3ara1n@outlook.com>
2025-06-09 10:47:01 +0800

    feat(theme): 添加新的中性色调支持
    - 在`HuskuiTheme.axaml`中引入了新的中性色调资源文件`Colors.Accent.Neutral.axaml`
    - 新增`AccentColor`枚举以支持不同的颜色选项
    - 更新`HuskuiTheme`类以支持动态更改主题色调
    - 修改`Colors.axaml`以移除不必要的颜色引用并添加新的前景色定义

--- src/Huskui.Avalonia/HuskuiTheme.axaml.cs
+++ src/Huskui.Avalonia/HuskuiTheme.axaml.cs
@@ -1,12 +1,43 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using Avalonia;
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Styling;
 
 namespace Huskui.Avalonia;
 
 public class HuskuiTheme : Styles
 {
+    public static readonly StyledProperty<AccentColor> AccentProperty =
+        AvaloniaProperty.Register<HuskuiTheme, AccentColor>(nameof(Accent),
+                                                            AccentColor.Neutral,
+                                                            defaultBindingMode: BindingMode.OneTime);
+
+
     public HuskuiTheme()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    public AccentColor Accent
+    {
+        get => GetValue(AccentProperty);
+        set => SetValue(AccentProperty, value);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == AccentProperty)
+        {
+            var color = change.GetNewValue<AccentColor>();
+            var source = $"avares://Huskui.Avalonia/Themes/Colors.Accent.{color}.axaml";
+            Resources.MergedDictionaries[0] =
+                new ResourceInclude(new Uri("avares://Huskui.Avalonia", UriKind.Absolute))
+                {
+                    Source = new Uri(source, UriKind.Absolute)
+                };
+        }
     }
 }
