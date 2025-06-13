273b2a2fcfd549f399d8f6ba5a90b8e81a542e43
Dearain <d3ara1n@outlook.com>
2025-06-13 10:50:06 +0800

    feat(AccentColor): 添加更多配色选项
    - 将 `AccentColor` 枚举从 7 种颜色扩展到 26 种颜色
    - 新增颜色包括 `Tomato`, `Red`, `Ruby`, `Crimson`, `Pink`, `Purple`, `Iris`, `Indigo`, `Cyan`, `Teal`, `Jade`, `Grass`, `Bronze`, `Gold`, `Orange`, `Yellow`, `Mint` 和 `Sky`
    - 提供更多的配色选项以增强用户界面设计灵活性

--- src/Huskui.Avalonia/AccentColor.cs
+++ src/Huskui.Avalonia/AccentColor.cs
@@ -1,3 +1,30 @@
 ﻿namespace Huskui.Avalonia;
 
-public enum AccentColor { Neutral, Amber, Blue, Lime, Plum, Sky, Violet }
+public enum AccentColor
+{
+    Neutral,
+    Tomato,
+    Red,
+    Ruby,
+    Crimson,
+    Pink,
+    Plum,
+    Purple,
+    Violet,
+    Iris,
+    Indigo,
+    Blue,
+    Cyan,
+    Teal,
+    Jade,
+    Green,
+    Grass,
+    Bronze,
+    Gold,
+    Orange,
+    Amber,
+    Yellow,
+    Lime,
+    Mint,
+    Sky
+}
