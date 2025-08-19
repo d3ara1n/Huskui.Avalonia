bb304eedff2ac7a5cfcf69e042ff1fbd948da905
d3ara1n <d3ara1n@outlook.com>
2025-08-19 16:38:04 +0800

    feat(Controls): 部分完成stepcontrol

--- src/Huskui.Avalonia/Controls/StepControl.cs
+++ src/Huskui.Avalonia/Controls/StepControl.cs
@@ -1,6 +1,55 @@
-﻿using Avalonia.Controls.Primitives;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Input;
+using Avalonia.Layout;
 
 namespace Huskui.Avalonia.Controls
 {
-    public class StepControl : SelectingItemsControl { }
+    public class StepControl : SelectingItemsControl
+    {
+        private static readonly FuncTemplate<Panel?> DefaultPanel =
+            new(() => new StackPanel { Orientation = Orientation.Horizontal });
+
+        protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey) =>
+            new StepItem();
+
+        protected override bool NeedsContainerOverride(object? item, int index, out object? recycleKey) =>
+            NeedsContainer<StepItem>(item, out recycleKey);
+
+        // protected override void ClearContainerForItemOverride(Control element) => UpdateSelectedIndex();
+        //
+        // protected override void PrepareContainerForItemOverride(Control container, object? item, int index) =>
+        //     UpdateSelectedIndex();
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == SelectedIndexProperty)
+            {
+                UpdateSelectedIndex();
+            }
+        }
+
+        private void UpdateSelectedIndex()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (ContainerFromIndex(i) is not StepItem item)
+                {
+                    continue;
+                }
+
+                item.IsCompleted = i < SelectedIndex;
+            }
+        }
+
+        static StepControl()
+        {
+            ItemsPanelProperty.OverrideDefaultValue<StepControl>(DefaultPanel);
+            KeyboardNavigation.TabNavigationProperty.OverrideDefaultValue<StepControl>(KeyboardNavigationMode.Once);
+        }
+    }
 }
--- src/Huskui.Avalonia/Controls/StepItem.cs
+++ src/Huskui.Avalonia/Controls/StepItem.cs
@@ -1,4 +1,42 @@
-﻿namespace Huskui.Avalonia.Controls
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Mixins;
+using Avalonia.Controls.Primitives;
+
+namespace Huskui.Avalonia.Controls
 {
-    public class StepItem { }
+    [PseudoClasses(":selected", ":completed")]
+    public class StepItem : HeaderedContentControl, ISelectable
+    {
+        public static readonly StyledProperty<bool> IsSelectedProperty = SelectingItemsControl.IsSelectedProperty
+           .AddOwner<ListBoxItem>();
+
+        public static readonly StyledProperty<bool> IsCompletedProperty =
+            AvaloniaProperty.Register<StepItem, bool>(nameof(IsCompleted));
+
+        public bool IsSelected
+        {
+            get => GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
+        public bool IsCompleted
+        {
+            get => GetValue(IsCompletedProperty);
+            set => SetValue(IsCompletedProperty, value);
+        }
+
+        static StepItem() => SelectableMixin.Attach<StepItem>(IsSelectedProperty);
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == IsCompletedProperty)
+            {
+                PseudoClasses.Set(":completed", change.GetNewValue<bool>());
+            }
+        }
+    }
 }
