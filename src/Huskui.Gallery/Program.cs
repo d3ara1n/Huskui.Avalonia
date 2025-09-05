67716e04992a68be38d7b7e95e3c70e6013e88a6
Dearain <d3ara1n@outlook.com>
2025-09-05 14:13:22 +0800

    fix(ui): 修改字体设置和日志记录方式
    - 将`Program.cs`中的日志记录方式从`LogToTrace`更改为`LogToTextWriter(Console.Out)`
    - 移除多个`axaml.cs`文件的编译指令，简化项目结构
    - 在`ToolTip.axaml`中修改了边框效果，增加了阴影效果
    - 在`CodeViewer.axaml`和`HomePage.axaml`中移除了字体设置，使用默认字体

--- src/Huskui.Gallery/Program.cs
+++ src/Huskui.Gallery/Program.cs
@@ -24,6 +24,6 @@ namespace Huskui.Gallery
                 })
                .With(new FontManagerOptions { DefaultFamilyName = "fonts:Manrope#Manrope" })
                .UsePlatformDetect()
-               .LogToTrace();
+               .LogToTextWriter(Console.Out);
     }
 }
