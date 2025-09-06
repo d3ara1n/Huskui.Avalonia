e78501730e5993f20d044cc5158188336cd5b8d7
Dearain <d3ara1n@outlook.com>
2025-09-06 18:54:06 +0800

    fix(Drawer): 修复样式被派生类覆盖

--- src/Huskui.Avalonia/Controls/Drawer.cs
+++ src/Huskui.Avalonia/Controls/Drawer.cs
@@ -4,6 +4,8 @@ namespace Huskui.Avalonia.Controls;
 
 public class Drawer : ContentControl
 {
+    protected override Type StyleKeyOverride => typeof(Drawer);
+
     public void Dismiss()
     {
         RaiseEvent(new OverlayItem.DismissRequestedEventArgs(this));
