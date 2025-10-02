442a0757f021570cc1dd5fc8fea8b176bb25a5ca
Dearain <d3ara1n@outlook.com>
2025-10-02 16:24:25 +0800

    refactor(Controls): 优化Modal弹出的视觉效果

--- src/Huskui.Avalonia/Converters/CornerRadiusConverters.cs
+++ src/Huskui.Avalonia/Converters/CornerRadiusConverters.cs
@@ -46,4 +46,59 @@ public static class CornerRadiusConverters
 
         return v;
     });
+
+    public static IValueConverter ToInnerRadius { get; } = new RelayConverter((v, p) =>
+    {
+        if (v is CornerRadius radius)
+        {
+            double? padding = p switch
+            {
+                double it => it,
+                int it => (double)it,
+                string it when double.TryParse(it, out var result) => result,
+                string it when int.TryParse(it, out var result) => (double)result,
+                Thickness { IsUniform: true } it => it.Bottom,
+                _ => null
+            };
+
+            if (padding.HasValue)
+            {
+                return new CornerRadius(radius.TopLeft - padding.Value,
+                                        radius.TopRight - padding.Value,
+                                        radius.BottomRight - padding.Value,
+                                        radius.BottomLeft - padding.Value);
+            }
+        }
+
+        return v;
+    });
+
+    public static IMultiValueConverter ToInnerRadiusMulti { get; } = new RelayMultiConverter((v, _, info) =>
+    {
+        return v switch
+        {
+            [CornerRadius c, double p] => new CornerRadius(c.TopLeft - p,
+                                                           c.TopRight - p,
+                                                           c.BottomRight - p,
+                                                           c.BottomLeft - p),
+            [CornerRadius c, int p] => new CornerRadius(c.TopLeft - p,
+                                                        c.TopRight - p,
+                                                        c.BottomRight - p,
+                                                        c.BottomLeft - p),
+            [CornerRadius c, string p] when double.TryParse(p, out var result) => new CornerRadius(c.TopLeft - result,
+                c.TopRight - result,
+                c.BottomRight - result,
+                c.BottomLeft - result),
+            [CornerRadius c, string p] when int.TryParse(p, out var result) => new CornerRadius(c.TopLeft - result,
+                c.TopRight - result,
+                c.BottomRight - result,
+                c.BottomLeft - result),
+            [CornerRadius c, Thickness { IsUniform: true } t] => new CornerRadius(c.TopLeft - t.Bottom,
+                c.TopRight - t.Bottom,
+                c.BottomRight - t.Bottom,
+                c.BottomLeft - t.Bottom),
+
+            _ => v
+        };
+    });
 }
---
+++ src/Huskui.Avalonia/Transitions/ModalTransition.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Animation.Easings;
+using Avalonia.Media;
+
+namespace Huskui.Avalonia.Transitions;
+
+public class ModalTransition : PageTransitionBase
+{
+    protected override void Configure(Builder from, Builder to, Lazy<Visual> parentAccessor)
+    {
+        from
+           .Animation(new SineEaseIn())
+           .AddFrame(0d,
+            [
+                (Visual.OpacityProperty, 1d), (ScaleTransform.ScaleXProperty, 1d), (ScaleTransform.ScaleYProperty, 1d)
+            ])
+           .AddFrame(1d,
+            [
+                (Visual.OpacityProperty, 0d),
+                (ScaleTransform.ScaleXProperty, 0.94d),
+                (ScaleTransform.ScaleYProperty, 0.94d)
+            ]);
+
+        to
+           .Animation(new BackEaseOut())
+           .AddFrame(0d, [(ScaleTransform.ScaleXProperty, 0.94d), (ScaleTransform.ScaleYProperty, 0.94d)])
+           .AddFrame(1d, [(ScaleTransform.ScaleXProperty, 1d), (ScaleTransform.ScaleYProperty, 1d)]);
+
+        to
+           .Animation(new SineEaseOut())
+           .AddFrame(0d, [(Visual.OpacityProperty, 0d)])
+           .AddFrame(1d, [(Visual.OpacityProperty, 1d)]);
+    }
+}
