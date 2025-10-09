ef80f4e3913d61c281d26500e4da810477069c02
Dearain <d3ara1n@outlook.com>
2025-10-09 13:04:05 +0800

    feat: add FadeTransition

---
+++ src/Huskui.Avalonia/Transitions/FadeTransition.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Animation.Easings;
+using Avalonia.Media;
+
+namespace Huskui.Avalonia.Transitions;
+
+public class FadeTransition : PageTransitionBase
+{
+    protected override void Configure(Builder from, Builder to, Lazy<Visual> parentAccessor)
+    {
+        from
+           .Animation(new QuarticEaseInOut())
+           .AddFrame(0d,
+            [
+                (Visual.OpacityProperty, 1d),
+                (ScaleTransform.ScaleXProperty, 1.0d),
+                (ScaleTransform.ScaleYProperty, 1.0d)
+            ])
+           .AddFrame(1d,
+            [
+                (Visual.OpacityProperty, 0d),
+                (ScaleTransform.ScaleXProperty, 0.98d),
+                (ScaleTransform.ScaleYProperty, 0.98d)
+            ]);
+        to
+           .Animation(new QuarticEaseInOut())
+           .AddFrame(0d,
+            [
+                (Visual.OpacityProperty, 0d),
+                (ScaleTransform.ScaleXProperty, 0.98d),
+                (ScaleTransform.ScaleYProperty, 0.98d)
+            ])
+           .AddFrame(1d,
+            [
+                (Visual.OpacityProperty, 1d), (ScaleTransform.ScaleXProperty, 1d), (ScaleTransform.ScaleYProperty, 1d)
+            ]);
+    }
+}
