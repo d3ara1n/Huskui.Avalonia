ee7a7d9123aeb6358c4d69e575eaf26cff0ff001
Dearain <d3ara1n@outlook.com>
2026-01-14 12:53:11 +0800

    feat: add Is & IsNot to ObjectConverters

--- src/Huskui.Avalonia/Converters/ObjectConverters.cs
+++ src/Huskui.Avalonia/Converters/ObjectConverters.cs
@@ -9,4 +9,10 @@ public static class ObjectConverters
 
     public static IValueConverter NotMatch { get; } =
         new RelayConverter((v, p) => !RelayConverter.CompareValues(v, p, v?.GetType()));
+
+    public static IValueConverter Is { get; } =
+        new RelayConverter((v, p) => v is not null && p is Type type ? v.GetType() == type : v);
+
+    public static IValueConverter IsNot { get; } =
+        new RelayConverter((v, p) => v is not null && p is Type type ? v.GetType() != type : v);
 }
