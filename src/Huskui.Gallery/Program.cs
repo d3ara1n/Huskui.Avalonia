3693278c4ab0ba79617ed085b63030b5b845ce0d
Dearain <d3ara1n@outlook.com>
2026-03-21 18:48:17 +0800

    format(format): format

--- src/Huskui.Gallery/Program.cs
+++ src/Huskui.Gallery/Program.cs
@@ -11,19 +11,24 @@ internal class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp() =>
         AppBuilder
-           .Configure<App>()
-           .ConfigureFonts(fontManager =>
+            .Configure<App>()
+            .ConfigureFonts(fontManager =>
             {
-                fontManager.AddFontCollection(new EmbeddedFontCollection(new("fonts:Manrope"),
-                                                                         new("avares://Huskui.Gallery/Assets/Fonts/Manrope")));
+                fontManager.AddFontCollection(
+                    new EmbeddedFontCollection(
+                        new("fonts:Manrope"),
+                        new("avares://Huskui.Gallery/Assets/Fonts/Manrope")
+                    )
+                );
             })
-           .With(new FontManagerOptions { DefaultFamilyName = "fonts:Manrope#Manrope" })
-           .UsePlatformDetect()
-           .LogToTextWriter(Console.Out)
-           .WithDeveloperTools();
+            .With(new FontManagerOptions { DefaultFamilyName = "fonts:Manrope#Manrope" })
+            .UsePlatformDetect()
+            .LogToTextWriter(Console.Out)
+            .WithDeveloperTools();
 }
