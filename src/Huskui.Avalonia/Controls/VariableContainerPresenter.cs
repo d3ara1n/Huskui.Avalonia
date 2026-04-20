93ad0d9e62ff757344191a9a50ca67919b3e3b7c
Dearain <d3ara1n@outlook.com>
2026-04-20 20:33:57 +0800

    feat(Controls): 神了，jb idea提交的时候默认不带新增文件

---
+++ src/Huskui.Avalonia/Controls/VariableContainerPresenter.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls.Presenters;
+using Avalonia.Data;
+
+namespace Huskui.Avalonia.Controls;
+
+public class VariableContainerPresenter : ContentPresenter
+{
+    public VariableContainerPresenter()
+    {
+        Bind(ContentProperty,
+             new Binding(nameof(Content))
+             {
+                 RelativeSource =
+                     new(RelativeSourceMode.FindAncestor) { AncestorType = typeof(VariableContainer), },
+             });
+
+        Bind(ContentTemplateProperty,
+             new Binding(nameof(ContentTemplate))
+             {
+                 RelativeSource =
+                     new(RelativeSourceMode.FindAncestor) { AncestorType = typeof(VariableContainer), },
+             });
+    }
+}
---
+++ src/Huskui.Gallery/Views/VariableContainerPage.axaml.cs
@@ -0,0 +1,8 @@
+using Huskui.Gallery.Controls;
+
+namespace Huskui.Gallery.Views;
+
+public partial class VariableContainerPage : ControlPage
+{
+    public VariableContainerPage() => InitializeComponent();
+}
